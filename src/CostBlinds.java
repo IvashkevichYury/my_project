7b06b17c7a0b43baf92cc5b4bc1081856f7a8dbf
Yury <iys@tut.by>
2021-12-26 21:08:27 +0300

    moved full price calculation to class CostBlinds

--- src/CostBlinds.java
+++ src/CostBlinds.java
@@ -1,13 +1,14 @@
+import java.util.Map;
+
 public class CostBlinds {
 
     private static final double dollarExchangeRate = 15;
 
     AreaBlinds areaBlinds = new AreaBlinds();
-    Data data = new Data();
 
-    public long calculateCostOfBlinds(Blinds blinds) {
+    public long calculateCostOfBlinds(Blinds blinds, Map<Integer, Double> priceForColors) {
 
-        long cost = Math.round(areaBlinds.findAreaBlinds(blinds) * data.determineCostOfSelectedColorOfBlinds(blinds.getColor())
+        long cost = Math.round(areaBlinds.findAreaBlinds(blinds) * priceForColors.get(blinds.getColor())
                 * dollarExchangeRate);
 
         return cost;
--- src/Data.java
+++ src/Data.java
@@ -4,22 +4,11 @@ import java.util.Map;
 public class Data {
 
     Map<Integer, Double> colorMap = new HashMap<>();
-    private double priceForColors;
-
-    public double determineCostOfSelectedColorOfBlinds(int color) {
-
-        initDate();
-        try {
-            priceForColors = colorMap.get(color);
-        } catch (NullPointerException e) {
-            System.out.println("The selected color doesn't exist!");
-        }
-        return priceForColors;
-    }
 
     public Map<Integer, Double> initDate() {
         colorMap.put(201, 8.8);
         colorMap.put(202, 10.4);
+
         return colorMap;
     }
 }
