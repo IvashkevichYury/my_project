fa796ab1421da3ea1d52bf6b06d2590373ce163e
Yury <iys@tut.by>
2021-12-26 21:40:33 +0300

    add scanner.close() in class UserPage

--- src/UserPage.java
+++ src/UserPage.java
@@ -4,17 +4,19 @@ public class UserPage {
 
     CostBlinds costBlinds = new CostBlinds();
     PriceCatalog priceCatalog = new PriceCatalog();
+    Scanner scanner = new Scanner(System.in);
 
     public void showBlindCost() {
         priceCatalog.initDate();
         while (true) {
             System.out.println("Would you like to calculate the cost of blinds?\nIf yes - enter Y, if no - enter N");
-            String answer = requestString();
+            String answer = scanner.nextLine();
             if (answer.equalsIgnoreCase("Y")) {
 
                 System.out.println("Blinds costs " + costBlinds.calculateCostOfBlinds(requestData(), priceCatalog.colorMap) + " rubles.\n");
             } else if (answer.equalsIgnoreCase("N")) {
                 System.out.println("Calculation finished.");
+                scanner.close();
                 break;
             }
         }
@@ -23,26 +25,11 @@ public class UserPage {
     public Blinds requestData() {
         Blinds blinds = new Blinds();
         System.out.println("Enter the width of the blind in mm: ");
-        blinds.setBlindsWidth(requestDouble() / 1000);
+        blinds.setBlindsWidth(scanner.nextDouble() / 1000);
         System.out.println("Enter the height of the blind in mm:");
-        blinds.setBlindsHeight(requestDouble() / 1000);
+        blinds.setBlindsHeight(scanner.nextDouble() / 1000);
         System.out.println("Enter the color number of the blinds (201, 202): ");
-        blinds.setColor(requestInteger());
+        blinds.setColor(scanner.nextInt());
         return blinds;
-    }
-
-    private int requestInteger() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-
-    private double requestDouble() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextDouble();
-    }
-
-    private String requestString() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
     }
 }
