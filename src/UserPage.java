6e3b3fcd1bbb073ae7f65130161b1a3ce0e77cc0
Yury <iys@tut.by>
2021-12-26 23:17:16 +0300

    added another scanner in class UserPage

--- src/UserPage.java
+++ src/UserPage.java
@@ -5,17 +5,19 @@ public class UserPage {
     CostBlinds costBlinds = new CostBlinds();
     PriceCatalog priceCatalog = new PriceCatalog();
     Scanner scanner = new Scanner(System.in);
+    Scanner scannerStr = new Scanner(System.in);
 
     public void showBlindCost() {
         priceCatalog.initDate();
         while (true) {
             System.out.println("Would you like to calculate the cost of blinds?\nIf yes - enter Y, if no - enter N");
-            String answer = scanner.nextLine();
+            String answer = scannerStr.nextLine();
             if (answer.equalsIgnoreCase("Y")) {
 
                 System.out.println("Blinds costs " + costBlinds.calculateCostOfBlinds(requestData(), priceCatalog.colorMap) + " rubles.\n");
             } else if (answer.equalsIgnoreCase("N")) {
                 System.out.println("Calculation finished.");
+                scannerStr.close();
                 scanner.close();
                 break;
             }
