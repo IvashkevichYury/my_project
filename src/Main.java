ede4e4db3e1f40665b1a42e52d1161b9738dbf27
Yury <iys@tut.by>
2021-12-27 16:25:53 +0300

    package ui renamed to package view

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import ui.UserPage;
+import view.UserPage;
 
 public class Main {
     public static void main(String[] args) {
--- src/view/UserPage.java
+++ src/view/UserPage.java
@@ -1,4 +1,4 @@
-package ui;
+package view;
 
 import model.Blind;
 import service.CostBlinds;
