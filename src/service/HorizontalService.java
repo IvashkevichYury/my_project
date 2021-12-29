4235e3dab4f0d98acab86161a751d66d521def88
Yury <iys@tut.by>
2021-12-29 16:40:39 +0300

    method calculateAreaOfBlinds removed from interface HorizontalService

--- src/service/HorizontalService.java
+++ src/service/HorizontalService.java
@@ -4,6 +4,5 @@ import model.Blind;
 
 public interface HorizontalService {
 
-    double calculateAreaOfBlinds(int width, int height);
     long calculateCost(Blind blind, PriceCatalog priceCatalogImpl);
 }
--- src/service/HorizontalServiceImpl.java
+++ src/service/HorizontalServiceImpl.java
@@ -7,8 +7,7 @@ public class HorizontalServiceImpl implements HorizontalService {
     private static final double dollarExchangeRate = 15;
     private double areaBlind;
 
-    @Override
-    public double calculateAreaOfBlinds(int width, int height) {
+    private double calculateAreaOfBlinds(int width, int height) {
         areaBlind = (width / 1000.00) * (height / 1000.00);
         if (areaBlind < 0.75) {
             areaBlind = 0.75;
