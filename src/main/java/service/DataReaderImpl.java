4f6021e7833d4fae9c20c5a7b40438c5db8b0ddf
Yury <iys@tut.by>
2022-01-24 17:29:37 +0300

    reading file to the map with Stream

--- src/main/java/service/DataReaderImpl.java
+++ src/main/java/service/DataReaderImpl.java
@@ -2,6 +2,7 @@ package service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -16,9 +17,12 @@ public class DataReaderImpl implements DataReader {
         try {
             Scanner scanner = new Scanner(file);
             scanner.nextLine();
+
             while (scanner.hasNextLine()) {
                 String[] lines = scanner.nextLine().split(",", 2);
-                price.put(Integer.parseInt(lines[0]), Double.parseDouble(lines[1]));
+                Arrays.stream(lines).forEach(s -> {
+                    price.putIfAbsent(Integer.parseInt(lines[0]), Double.parseDouble(lines[1]));
+                });
             }
         } catch (FileNotFoundException e) {
             System.out.println("Файл " + fileName + " не найден!");
