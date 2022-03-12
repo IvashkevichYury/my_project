ce5e4b3864814932a77c50af0d50f32d79f2ed10
Yury <iys@tut.by>
2022-03-12 12:34:07 +0300

    changed readPricesFromFile()

--- src/main/java/service/fileService/DataReaderImpl.java
+++ src/main/java/service/fileService/DataReaderImpl.java
@@ -6,22 +6,21 @@ import com.opencsv.exceptions.CsvException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class DataReaderImpl implements DataReader {
 
     @Override
     public Map<Integer, Double> readPricesFromFile(String fileName) {
-        Map<Integer, Double> price = new HashMap<>();
         try (CSVReader reader = new CSVReader(new FileReader(fileName));) {
-            reader.readAll()
+            return reader.readAll()
                     .stream()
                     .skip(1)
-                    .forEach((a) -> {
-                        price.put(Integer.parseInt(a[0]), Double.parseDouble(a[1]));
-                    });
+                    .collect(Collectors.toMap(k -> Integer.parseInt(k[0]), v -> Double.parseDouble(v[1])));
+
         } catch (IOException | CsvException e) {
             e.printStackTrace();
+            return null;
         }
-        return price;
     }
 }
